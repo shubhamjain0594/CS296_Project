31c2ec31724c8ac6de83e6f19bb40377452dc0e3
shubhamjain0594 <shubhamjain0594@gmail.com>
2014-04-04 00:01:20 +0530

    Changes to shubhamj

--- cs296-02-lab03/cs296_base_code/src/cs296_base.cpp
+++ cs296-02-lab03/cs296_base_code/src/cs296_base.cpp
@@ -15,7 +15,7 @@
 * misrepresented as being the original software.
 * 3. This notice may not be removed or altered from any source distribution.
 */
-
+#include "dominos.hpp"
 #include "cs296_base.hpp"
 #include <cstdio>
 using namespace std;
